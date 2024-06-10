e18ba59da207fa37334fdc530253c720ff8d7bc9
nancyannelouis <162673352+nancyannelouis@users.noreply.github.com>
2024-06-11 07:01:27 +0900

    Add files via upload

---
+++ file.js
@@ -0,0 +1,12 @@
+dd = 10;
+{
+    let aa = 999;
+    console.log(aa);
+    aa= "aaaaa";
+    console.log(aa);
+}
+alert('welcome to blackpink world!');
+//alert(document.cookie);
+//alert(window.location);
+//eval('window.location = "https://naver.com"')
+//alert(eval("1 + 1 "));
